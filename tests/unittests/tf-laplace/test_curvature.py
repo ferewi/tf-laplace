c46d090066772bbd3df1c9d75323ce2d65bdb8a6
Ferdinand Rewicki <ferdinand.rewicki@gmail.com>
2021-07-27 13:55:16 +0200

    added test for KFAC

--- tests/unittests/tf-laplace/test_curvature.py
+++ tests/unittests/tf-laplace/test_curvature.py
@@ -4,7 +4,7 @@ from unittest import mock
 import tensorflow as tf
 import numpy as np
 import numpy.testing as npt
-from laplace.curvature import LayerMap, DiagFisher, BlockDiagFisher
+from laplace.curvature import LayerMap, DiagFisher, BlockDiagFisher, KFAC
 
 
 @unittest.skip('dev')
@@ -115,7 +115,7 @@ class RealTfModel:
 
     def __init__(self, model):
         self.model = model
-        self.input = tf.ones([1,2]) * 1
+        self.input = tf.ones([1, 2]) * 1
         self.y_true = [[9.]]
         self.loss = tf.keras.losses.MeanSquaredError()
 
@@ -160,6 +160,7 @@ class ModelMocker:
         model = mock.create_autospec(tf.keras.models.Sequential)
         return model
 
+
 @unittest.skip('dev')
 class DiagFisherTest(unittest.TestCase):
 
@@ -181,13 +182,13 @@ class DiagFisherTest(unittest.TestCase):
         diagfisher.update(gradients, 1)
 
         # then
-        stats = {
+        expected_state = {
             'dense': [[4., 4., 4.], [4., 4., 4.], [4., 4., 4.]],
             'dense_1': [[9., 9.], [9., 9.], [9., 9.], [1., 1.]]
         }
         self.assertEqual(len(diagfisher.state), 2)
         for lname, litem in diagfisher.state.items():
-            npt.assert_allclose(litem.numpy(), stats[lname])
+            npt.assert_allclose(litem.numpy(), expected_state[lname])
 
     def test_update_second_iteration(self):
         # given
@@ -211,19 +212,16 @@ class DiagFisherTest(unittest.TestCase):
         diagfisher.update(gradients, 1)
 
         # then
-        stats = {
+        expected_state = {
             'dense': np.array([[5., 5., 5.], [5., 5., 5.], [5., 5., 5.]]),
             'dense_1': np.array([[10., 10.], [10., 10.], [10., 10.], [2., 2.]])
         }
         self.assertEqual(len(diagfisher.state), 2)
         for lname, litem in diagfisher.state.items():
-            npt.assert_allclose(litem.numpy(), stats[lname])
+            npt.assert_allclose(litem.numpy(), expected_state[lname])
 
-    @patch('tensorflow.math.reciprocal')
-    def test_invert(self, reci_func):
+    def test_invert(self):
         # given
-        reci_func.side_effect = lambda x: 1./x
-
         model = ModelMocker.mock_model()
         layer1 = ModelMocker.mock_layer('dense', (2, 3))
         layer2 = ModelMocker.mock_layer('dense_1', (3, 2))
@@ -251,10 +249,11 @@ class DiagFisherTest(unittest.TestCase):
         for lname, litem in inverse.items():
             npt.assert_allclose(litem.numpy(), expected_inverse[lname])
 
-
+@unittest.skip('dev')
 class BlockDiagFisherTest(unittest.TestCase):
 
     def test_update_first_iteration(self):
+        # given
         model = ModelMocker.mock_model()
         layer1 = ModelMocker.mock_layer('dense', (2, 3))
         layer2 = ModelMocker.mock_layer('dense_1', (3, 2))
@@ -271,15 +270,15 @@ class BlockDiagFisherTest(unittest.TestCase):
         blockfisher.update(gradients, 1)
 
         # then
-        stats = {
+        expected_state = {
             'dense': np.ones([9, 9])*4,
             'dense_1': np.repeat([[9., 9., 9., 9., 9., 9., 3., 3.]], 8, axis=0)
         }
-        stats['dense_1'][6] = stats['dense_1'][6]/3
-        stats['dense_1'][7] = stats['dense_1'][7]/3
+        expected_state['dense_1'][6] = expected_state['dense_1'][6]/3
+        expected_state['dense_1'][7] = expected_state['dense_1'][7]/3
         self.assertEqual(len(blockfisher.state), 2)
         for lname, litem in blockfisher.state.items():
-            npt.assert_allclose(litem.numpy(), stats[lname])
+            npt.assert_allclose(litem.numpy(), expected_state[lname])
 
     def test_update_second_iteration(self):
         # given
@@ -303,21 +302,18 @@ class BlockDiagFisherTest(unittest.TestCase):
         blockfisher.update(gradients, 1)
 
         # then
-        stats = {
+        expected_state = {
             'dense': np.ones([9, 9]) * 5,
             'dense_1': np.repeat([[10., 10., 10., 10., 10., 10., 4., 4.]], 8, axis=0)
         }
-        stats['dense_1'][6] = [4., 4., 4., 4., 4., 4., 2., 2.]
-        stats['dense_1'][7] = [4., 4., 4., 4., 4., 4., 2., 2.]
+        expected_state['dense_1'][6] = [4., 4., 4., 4., 4., 4., 2., 2.]
+        expected_state['dense_1'][7] = [4., 4., 4., 4., 4., 4., 2., 2.]
         self.assertEqual(len(blockfisher.state), 2)
         for lname, litem in blockfisher.state.items():
-            npt.assert_allclose(litem.numpy(), stats[lname])
+            npt.assert_allclose(litem.numpy(), expected_state[lname])
 
-    @patch('tensorflow.math.reciprocal')
-    def test_invert(self, reci_func):
+    def test_invert(self):
         # given
-        reci_func.side_effect = lambda x: 1./x
-
         model = ModelMocker.mock_model()
         layer1 = ModelMocker.mock_layer('dense', (2, 3))
         layer2 = ModelMocker.mock_layer('dense_1', (3, 2))
@@ -344,6 +340,129 @@ class BlockDiagFisherTest(unittest.TestCase):
         self.assertEqual(len(inverse), 2)
         for lname, litem in inverse.items():
             npt.assert_allclose(litem.numpy(), expected_inverse[lname])
+
+
+class KFACTest(unittest.TestCase):
+    def test_update_first_iteration(self):
+        # given
+        model = ModelMocker.mock_model()
+        layer1 = ModelMocker.mock_layer('dense', (2, 3))
+        layer2 = ModelMocker.mock_layer('dense_1', (3, 2))
+        model.layers = [layer1, layer2]
+
+        kfac = KFAC(model)
+        kfac._layer_inputs['dense'] = [[1., 1.]]
+        kfac._layer_inputs['dense_1'] = [[3., 3., 3.]]
+        grads_preactivations = {
+            'dense': np.array([[18., 18., 18.]]),
+            'dense_1': np.array([[9., 9.]]),
+        }
+
+        # when
+        kfac.update(grads_preactivations, 1)
+
+        # then
+        expected_state = {
+            'dense': [
+                np.ones([3, 3]),
+                np.ones([3, 3]) * 324
+            ],
+            'dense_1': [
+                [[9., 9., 9., 3.], [9., 9., 9., 3.], [9., 9., 9., 3.], [3., 3., 3., 1.]],
+                [[81., 81.], [81., 81.]]
+            ]
+        }
+        self.assertEqual(len(kfac.state), 2)
+        for lname, litem in kfac.state.items():
+            Q, H = litem[0].numpy(), litem[1].numpy()
+            npt.assert_allclose(Q, expected_state[lname][0])
+            npt.assert_allclose(H, expected_state[lname][1])
+
+    def test_update_second_iteration(self):
+        # given
+        model = ModelMocker.mock_model()
+        layer1 = ModelMocker.mock_layer('dense', (2, 3))
+        layer2 = ModelMocker.mock_layer('dense_1', (3, 2))
+        model.layers = [layer1, layer2]
+
+        kfac = KFAC(model)
+        kfac._layer_inputs['dense'] = [[1., 1.]]
+        kfac._layer_inputs['dense_1'] = [[3., 3., 3.]]
+        grads_preactivations = {
+            'dense': np.array([[18., 18., 18.]]),
+            'dense_1': np.array([[9., 9.]]),
+        }
+        kfac.state = {
+            'dense': [np.ones([3, 3]), np.ones([3, 3])],
+            'dense_1': [np.ones([4, 4]), np.ones([2, 2])]
+        }
+
+        # when
+        kfac.update(grads_preactivations, 1)
+
+        # then
+        expected_state = {
+            'dense': [
+                np.ones([3, 3]) * 2,
+                np.ones([3, 3]) * 325
+            ],
+            'dense_1': [
+                [[10., 10., 10., 4.], [10., 10., 10., 4.], [10., 10., 10., 4.], [4., 4., 4., 2.]],
+                [[82., 82.], [82., 82.]]
+            ]
+        }
+        self.assertEqual(len(kfac.state), 2)
+        for lname, litem in kfac.state.items():
+            Q, H = litem[0].numpy(), litem[1].numpy()
+            npt.assert_allclose(Q, expected_state[lname][0])
+            npt.assert_allclose(H, expected_state[lname][1])
+
+    def test_invert(self):
+        # given
+        model = ModelMocker.mock_model()
+        layer1 = ModelMocker.mock_layer('dense', (2, 3))
+        layer2 = ModelMocker.mock_layer('dense_1', (3, 2))
+        model.layers = [layer1, layer2]
+
+        kfac = KFAC(model)
+        kfac.state = {
+            'dense': [
+                np.ones([3, 3]),
+                np.ones([3, 3]) * 324
+            ],
+            'dense_1': [
+                np.array([[9., 9., 9., 3.], [9., 9., 9., 3.], [9., 9., 9., 3.], [3., 3., 3., 1.]]),
+                np.array([[81., 81.], [81., 81.]])
+            ]
+        }
+
+        # when
+        inv = kfac.invert(norm=1., scale=1.)
+
+        # then
+        expected_inv = {
+            'dense': [
+                [[0.8660254, 0., 0.],
+                 [-0.28867513, 0.8164966, 0.],
+                 [-0.2886751, -0.40824828, 0.70710677]],
+                [[0.81670785, 0., 0.],
+                 [-0.40772474, 0.7076513, 0.],
+                 [-0.4077247, -0.70547396, 0.05546965]]
+            ],
+            'dense_1': [
+                [[0.8304549, 0., 0., 0.],
+                 [-0.3737047, 0.7416198, 0., 0.],
+                 [-0.37370473, -0.6067798, 0.42640147, 0.],
+                 [-0.12456819, -0.20225996, -0.6396021, 0.7071068]],
+                [[0.7092719, 0.],
+                 [-0.7006222, 0.11043184]],
+            ]
+        }
+        for lname, factors in inv.items():
+            Q, H = factors[0].numpy(), factors[1].numpy()
+            npt.assert_allclose(Q, expected_inv[lname][0])
+            npt.assert_allclose(H, expected_inv[lname][1])
+
 
 
 if __name__ == '__main__':
