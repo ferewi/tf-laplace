424202a7c4228d2edb74d5b393c9537551f51ea2
Ferdinand Rewicki <ferdinand.rewicki@gmail.com>
2021-07-27 17:20:56 +0200

    added sampler module together with tests

---
+++ laplace/sampler.py
@@ -0,0 +1,81 @@
+from abc import ABC, abstractmethod
+import tensorflow as tf
+from laplace.curvature import Curvature
+
+
+class Sampler(ABC):
+
+    def __init__(self, curvature: Curvature, norm: float, scale: float):
+        self._curvature = curvature
+        self._curvature.invert(norm, scale)
+        self.norm = norm
+        self.scale = scale
+
+    @staticmethod
+    def create(curvature: Curvature, norm: float, scale: float):
+        sampler_by_curvature = {
+            "DiagFisher": DiagFisherSampler,
+            "BlockDiagFisher": BlockDiagFisherSampler,
+            "KFAC": KFACSampler
+        }
+        return sampler_by_curvature[curvature.__class__.__name__](curvature, norm, scale)
+
+    @abstractmethod
+    def sample_and_replace_weights(self):
+        pass
+
+    def _split_kernel_bias_weights_and_replace(self, layer: tf.keras.layers.Layer, x: tf.Tensor):
+        if len(layer.weights) == 1:
+            x_k, x_b = x, tf.zeros(shape=(), dtype=tf.float32)
+            pass
+        elif len(layer.weights) == 2:
+            x_k, x_b = tf.split(x, [x.shape[0] - 1, 1], axis=0)
+            pass
+        else:
+            raise RuntimeError(f"Can't determine kernel/bias weights split for layer {layer},"
+                               f" having {len(layer.weights)} weight sets.")
+        new_weights = dict()
+        for weights in layer.weights:
+            if self._curvature.layer_map.is_bias(weights.name):
+                new_weights[weights.name] = tf.add(weights, tf.squeeze(x_b))
+            else:
+                x_k = tf.reshape(x_k, weights.shape)
+                new_weights[weights.name] = tf.add(weights, x_k)
+
+        layer.set_weights(list(new_weights.values()))
+
+
+class DiagFisherSampler(Sampler):
+
+    def sample_and_replace_weights(self):
+        for layer in self._curvature.model.layers:
+            if self._curvature.layer_map.is_curvature_eligible(layer.name):
+                layer_id = layer.name.split('/')[0]
+                var = self._curvature.get_inverse(layer_id)
+                x = tf.random.normal(shape=var.shape) * var
+                self._split_kernel_bias_weights_and_replace(layer, x)
+
+
+class BlockDiagFisherSampler(Sampler):
+
+    def sample_and_replace_weights(self):
+        for layer in self._curvature.model.layers:
+            if self._curvature.layer_map.is_curvature_eligible(layer.name):
+                layer_id = layer.name.split('/')[0]
+                layer_shape = self._curvature.layer_map.get_layer_shape(layer.name)
+                var = self._curvature.get_inverse(layer_id)
+                x = tf.linalg.matvec(var, tf.random.normal(shape=[var.shape[0]]))
+                x = tf.reshape(x, layer_shape)
+                self._split_kernel_bias_weights_and_replace(layer, x)
+
+
+class KFACSampler(Sampler):
+
+    def sample_and_replace_weights(self):
+        for layer in self._curvature.model.layers:
+            if self._curvature.layer_map.is_curvature_eligible(layer.name):
+                layer_id = layer.name.split('/')[0]
+                a, b = self._curvature.get_inverse(layer_id)
+                z = tf.random.normal(shape=[a.shape[0], b.shape[0]])
+                x = tf.matmul(tf.matmul(a, z), b, transpose_b=True)
+                self._split_kernel_bias_weights_and_replace(layer, x)
---
+++ tests/unittests/tf-laplace/test_sampler.py
@@ -0,0 +1,144 @@
+import unittest
+from unittest.mock import patch
+import numpy as np
+import tensorflow as tf
+
+from laplace.curvature import DiagFisher, BlockDiagFisher, KFAC
+from laplace.sampler import Sampler, DiagFisherSampler, BlockDiagFisherSampler, KFACSampler
+from tests.testutils.tensorflow import ModelMocker, RealTfModel
+
+
+class SamplerTest(unittest.TestCase):
+
+    def test_it_creates_the_sampler_according_to_the_curvature(self):
+        # given
+        model = ModelMocker.mock_model()
+        layer1 = ModelMocker.mock_layer('dense', (2, 3))
+        layer2 = ModelMocker.mock_layer('dense_1', (3, 2))
+        model.layers = [layer1, layer2]
+
+        diagfisher = DiagFisher(model)
+        blockfisher = BlockDiagFisher(model)
+        kfac = KFAC(model)
+
+        # when
+        diagsampler = Sampler.create(diagfisher, norm=1., scale=1.)
+        blocksampler = Sampler.create(blockfisher, norm=1., scale=1.)
+        kfacsampler = Sampler.create(kfac, norm=1., scale=1.)
+
+        # then
+        self.assertIsInstance(diagsampler, DiagFisherSampler)
+        self.assertIsInstance(blocksampler, BlockDiagFisherSampler)
+        self.assertIsInstance(kfacsampler, KFACSampler)
+
+
+class DiagFisherSamplerTest(unittest.TestCase):
+
+    @patch('tensorflow.add')
+    def test_sample_and_replace_weights(self, add_func):
+        # given
+        model = ModelMocker.mock_model()
+        layer1 = ModelMocker.mock_layer('dense', (2, 3))
+        layer2 = ModelMocker.mock_layer('dense_1', (3, 2))
+        model.layers = [layer1, layer2]
+        add_func.side_effect = lambda x, y: y
+
+        diagfisher = DiagFisher(model)
+        diagfisher.state = {
+            'dense': np.array([[4., 4., 4.], [4., 4., 4.], [4., 4., 4.]], dtype=np.float32),
+            'dense_1': np.array([[9., 9.], [9., 9.], [9., 9.], [1., 1.]], dtype=np.float32)
+        }
+        sampler = DiagFisherSampler(diagfisher, norm=1., scale=1.)
+
+        # assert new weights have right shape
+        def assert_weight_layer_1(x):
+            self.assertEqual([2, 3], x[0].shape)
+            self.assertEqual([3, ], x[1].shape)
+
+        def assert_weight_layer_2(x):
+            self.assertEqual([3, 2], x[0].shape)
+            self.assertEqual([2, ], x[1].shape)
+        layer1.set_weights = assert_weight_layer_1
+        layer2.set_weights = assert_weight_layer_2
+
+        # when
+        sampler.sample_and_replace_weights()
+
+
+class BlockFisherSamplerTest(unittest.TestCase):
+
+    @patch('tensorflow.add')
+    def test_sample_and_replace_weights(self, add_func):
+        # given
+        model = ModelMocker.mock_model()
+        layer1 = ModelMocker.mock_layer('dense', (2, 3))
+        layer2 = ModelMocker.mock_layer('dense_1', (3, 2))
+        model.layers = [layer1, layer2]
+        add_func.side_effect = lambda x, y: y
+
+        blockfisher = BlockDiagFisher(model)
+        blockfisher.state = {
+            'dense': np.ones([9, 9]) * 4,
+            'dense_1': np.repeat([[9., 9., 9., 9., 9., 9., 3., 3.]], 8, axis=0)
+        }
+        blockfisher.state['dense_1'][6] = blockfisher.state['dense_1'][6] / 3
+        blockfisher.state['dense_1'][7] = blockfisher.state['dense_1'][7] / 3
+        sampler = BlockDiagFisherSampler(blockfisher, norm=1., scale=1.)
+
+        # assert new weights have right shape
+        def assert_weight_layer_1(x):
+            self.assertEqual([2, 3], x[0].shape)
+            self.assertEqual([3, ], x[1].shape)
+
+        def assert_weight_layer_2(x):
+            self.assertEqual([3, 2], x[0].shape)
+            self.assertEqual([2, ], x[1].shape)
+        layer1.set_weights = assert_weight_layer_1
+        layer2.set_weights = assert_weight_layer_2
+
+        # when
+        sampler.sample_and_replace_weights()
+
+
+class KFACSamplerTest(unittest.TestCase):
+
+    @patch('tensorflow.add')
+    def test_sample_and_replace_weights(self, add_func):
+        # given
+        model = ModelMocker.mock_model()
+        layer1 = ModelMocker.mock_layer('dense', (2, 3))
+        layer2 = ModelMocker.mock_layer('dense_1', (3, 2))
+        model.layers = [layer1, layer2]
+        add_func.side_effect = lambda x, y: y
+
+        kfac = KFAC(model)
+        kfac.state = {
+            'dense': [
+                np.ones([3, 3]),
+                np.ones([3, 3]) * 324
+            ],
+            'dense_1': [
+                np.array([[9., 9., 9., 3.], [9., 9., 9., 3.], [9., 9., 9., 3.], [3., 3., 3., 1.]]),
+                np.array([[81., 81.], [81., 81.]])
+            ]
+        }
+        sampler = KFACSampler(kfac, norm=1., scale=1.)
+
+        # assert new weights have right shape
+        def assert_weight_layer_1(x):
+            self.assertEqual([2, 3], x[0].shape)
+            self.assertEqual([3, ], x[1].shape)
+
+        def assert_weight_layer_2(x):
+            self.assertEqual([3, 2], x[0].shape)
+            self.assertEqual([2, ], x[1].shape)
+        layer1.set_weights = assert_weight_layer_1
+        layer2.set_weights = assert_weight_layer_2
+
+        # when
+        sampler.sample_and_replace_weights()
+
+
+
+if __name__ == '__main__':
+    unittest.main()
